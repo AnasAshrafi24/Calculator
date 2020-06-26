98a3a8561f9bd3e7077cd2216df2ccbbfaa48bf7
AnasAshrafi24 <Anas24ashrafi@gmail.com>
2020-06-26 11:04:54 -0700

    Calculator

---
+++ app.js
@@ -0,0 +1,21 @@
+function getNumber (num){
+    
+   let result = document.getElementById("result")
+
+   result.value += num ;
+}
+
+function clearResult(){
+    let result = document.getElementById("result")
+
+    result.value = ""
+
+}
+
+function getResult(){
+ 
+            let result = document.getElementById("result")
+
+            result.value = eval(result.value)
+
+}
